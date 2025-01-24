1cb4492726ac121b26a1a0a3247bf44829face07
João Estevam <joaovestevamr@gmail.com>
2025-01-24 20:20:32 -0300

    update: readme update

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -27,7 +27,7 @@ export class UserController {
     return this.userService.update(+id, updateUserDto);
   }
 
-  @Post('delete/:id')
+  @Delete('delete/:id')
   remove(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.remove(+id, updateUserDto);
   }
